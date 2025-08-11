50f4ad30c4f974fc67828b82ffdd97e2cfefd29b
ahmed123humran <ahm88m%0@gmail.com>
2025-08-11 13:04:53 +0300

    [ADD] SEO

---
+++ .next/static/chunks/src_app_[locale]_layout_tsx_0a548d63._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_5f8d3416._.css",
+  "static/chunks/node_modules_31dc3091._.js",
+  "static/chunks/src_09bc7331._.js"
+],
+    source: "dynamic"
+});
---
+++ .next/static/chunks/src_app_[locale]_page_tsx_0f628811._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_home_d6d411fd._.js"
+],
+    source: "dynamic"
+});
